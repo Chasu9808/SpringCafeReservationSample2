00fa448460531f31cb4800debec392324f37e21e
lsy3709 <lsy3709@naver.com>
2024-10-10 06:02:56 +0900

    상품 목록 조회 및 예약 작업 14,결제 버튼 클릭시 결제 페이지 연결 확인

--- app/src/main/java/com/sylovestp/firebasetest/testspringrestapp/itemListPaging/adapter/ItemListAdapter.kt
+++ app/src/main/java/com/sylovestp/firebasetest/testspringrestapp/itemListPaging/adapter/ItemListAdapter.kt
@@ -1,5 +1,6 @@
 package com.sylovestp.firebasetest.testspringrestapp.itemListPaging.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +9,11 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.iamport.sdk.domain.core.Iamport.response
 import com.sylovestp.firebasetest.testspringrestapp.R
 import com.sylovestp.firebasetest.testspringrestapp.databinding.ItemListViewBinding
 import com.sylovestp.firebasetest.testspringrestapp.itemListPaging.dto.ItemListDTO
+import com.sylovestp.firebasetest.testspringrestapp.pay.ui.MainActivity
 
 
 class ItemListAdapter : PagingDataAdapter<ItemListDTO, ItemListAdapter.ItemListViewHolder>(DIFF_CALLBACK) {
@@ -49,6 +52,26 @@ class ItemListAdapter : PagingDataAdapter<ItemListDTO, ItemListAdapter.ItemListV
             } else {
                 // Otherwise, make button invisible or gone
                 binding.payBtn.visibility = View.GONE
+            }
+
+            binding.payBtn.setOnClickListener {
+                // Intent 생성, 현재 context에서 새로운 Activity로 이동
+                val context = itemView.context
+                val intent = Intent(context, MainActivity::class.java)
+
+                // 데이터를 전달
+                intent.putExtra("reservationId", "${binding.item2ReservationId.text}")
+                intent.putExtra("reservationDate", "${binding.item2ReservationDate.text}")
+                intent.putExtra("reservationTime", "${ binding.itemReservationTime.text}")
+                intent.putExtra("username", "${binding.itemUsername.text}")
+                intent.putExtra("phone", "${binding.itemPhone.text}")
+                intent.putExtra("address", "${binding.itemAddress.text}")
+                intent.putExtra("name", "${ binding.itemName.text}")
+                intent.putExtra("price", "${ binding.itemPrice.text}")
+                // 추가적으로 전달할 데이터가 있으면 여기에 작성
+
+                // 새로운 Activity 시작
+                context.startActivity(intent)
             }
 
 //            val imageUrl = "http://10.100.201.87:8080/items/${item.itemRepImageId}/itemImageObjectId"
